403bc6f8e623a5c1402d0620d173cae23d397fb6
unknown <kyawzinhein162@gmail.com>
2024-05-10 06:47:12 +0630

    register add

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,10 +1,16 @@
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
+import { Bounce, ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 // formik custom error message
 import StyledErrorMessage from "./StyledErrorMessage";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const AuthForm = ({ isLogin }) => {
+  const [redirect, setRedirect] = useState(false);
   const initialValues = {
     username: "",
     email: "",
@@ -24,66 +30,115 @@ const AuthForm = ({ isLogin }) => {
       .required("Password is required!"),
   });
 
-  const submitHandler = (values) => {
-    console.log(values);
+  const submitHandler = async (values) => {
+    const { email, password, username } = values;
+    if (isLogin) {
+      // login codes
+    } else {
+      const response = await fetch(`${import.meta.env.VITE_API}/register`, {
+        method: "POST",
+        body: JSON.stringify({ email, password, username }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.status === 201) {
+        setRedirect(true);
+      } else if (response.status === 400) {
+        const data = await response.json();
+        const pickedMessage = data.errorMessage[0].msg;
+        toast.error(pickedMessage, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          transition: Bounce,
+        });
+      }
+    }
   };
 
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={authFormSchema}
-      onSubmit={submitHandler}
-    >
-      {() => (
-        <Form className="w-1/2 mx-auto">
-          <h1 className="text-center font-bold text-4xl my-4 text-teal-600">
-            {isLogin ? "Login" : "Register"}
-          </h1>
-          <div className="mb-3">
-            <label htmlFor="title" className="font-medium block">
-              Username
-            </label>
-            <Field
-              type="text"
-              name="username"
-              id="username"
-              className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
-            />
-            <StyledErrorMessage name="username" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="title" className="font-medium block">
-              Email
-            </label>
-            <Field
-              type="email"
-              name="email"
-              id="email"
-              className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
-            />
-            <StyledErrorMessage name="email" />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="title" className="font-medium block">
-              Password
-            </label>
-            <Field
-              type="password"
-              name="password"
-              id="password"
-              className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
-            />
-            <StyledErrorMessage name="password" />
-          </div>
-          <button
-            className="w-full bg-teal-600 font-medium text-white py-2 rounded-md mt-2"
-            type="submit"
-          >
-            {isLogin ? "Login" : "Register"}
-          </button>
-        </Form>
-      )}
-    </Formik>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition:Bounce
+      />
+      <Formik
+        initialValues={initialValues}
+        validationSchema={authFormSchema}
+        onSubmit={submitHandler}
+      >
+        {() => (
+          <Form className="w-1/2 mx-auto">
+            <h1 className="text-center font-bold text-4xl my-4 text-teal-600">
+              {isLogin ? "Login" : "Register"}
+            </h1>
+            {!isLogin && (
+              <div className="mb-3">
+                <label htmlFor="title" className="font-medium block">
+                  Username
+                </label>
+                <Field
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
+                />
+                <StyledErrorMessage name="username" />
+              </div>
+            )}
+            <div className="mb-3">
+              <label htmlFor="title" className="font-medium block">
+                Email
+              </label>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
+              />
+              <StyledErrorMessage name="email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="title" className="font-medium block">
+                Password
+              </label>
+              <Field
+                type="password"
+                name="password"
+                id="password"
+                className="text-lg border-2 border-teal-600 rounded-md py-2 w-full px-2 outline-none"
+              />
+              <StyledErrorMessage name="password" />
+            </div>
+            <button
+              className="w-full bg-teal-600 font-medium text-white py-2 rounded-md mt-2"
+              type="submit"
+            >
+              {isLogin ? "Login" : "Register"}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-slate-200 flex items-center justify-between px-10 mb-8">
-      <h1 className="text-3xl text-teal-600 font-bold py-2">
-        <a href="/">SHARENOTE</a>
-      </h1>
-      <div>
+      <Link to={"/"} className="text-3xl text-teal-600 font-bold py-2">
+        SHARENOTE
+      </Link>
+      <div className="flex gap-3">
         <Link to={"/create"} className="text-teal-600 font-medium">
           CREATE
+        </Link>
+        <Link to={"/login"} className="text-teal-600 font-medium">
+          Login
+        </Link>
+        <Link to={"/register"} className="text-teal-600 font-medium">
+          Register
         </Link>
       </div>
     </nav>
